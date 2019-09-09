9b9dadef544d20d94c2e9ea008ded5cca8dca0a1
ViniciusRio <viniciustrave@gmail.com>
2019-09-09 00:45:29 -0400

    add nova page myEvents

---
+++ src/app/pages/events/myEvents/myEvents.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+
+import { IonicModule } from '@ionic/angular';
+
+import { MyEventsPage } from './myEvents.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MyEventsPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [MyEventsPage]
+})
+export class MyEventsPageModule {}
---
+++ src/app/pages/events/myEvents/myEvents.page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-events',
+  templateUrl: './myEvents.page.html',
+})
+export class MyEventsPage {}
