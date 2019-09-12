72ee5d3b42c5c37d9f9d08acf50cb6dfc50146f8
ViniciusRio <viniciustrave@gmail.com>
2019-09-12 00:26:13 -0400

    criando service e model

---
+++ src/app/pages/events/event.model.ts
@@ -0,0 +1,11 @@
+export interface Event {
+    id: number;
+    name: string;
+    about: string;
+    briefInformation: string;
+    entertainment: string;
+    food: string;
+    price: number;
+    date: string;
+    urlImage: string;
+}
