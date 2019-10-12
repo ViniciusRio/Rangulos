054a364c848cf250e3692c6a4a9cb44c18e339b9
ViniciusRio <viniciustrave@gmail.com>
2019-10-11 22:23:51 -0400

    atualizado auth

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private userAuthenticate = false;
+  private userAuthenticate = true;
 
   get userAuth() {
     return this.userAuthenticate;
