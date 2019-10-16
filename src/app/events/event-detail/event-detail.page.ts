ca394e44764379e5e865ed49fff172aeff5927ba
ViniciusRio <viniciustrave@gmail.com>
2019-10-16 19:40:23 -0400

    modificando o exibição

--- src/app/events/event-detail/event-detail.page.ts
+++ src/app/events/event-detail/event-detail.page.ts
@@ -1,29 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Event } from '../event.model';
 import { EventsService } from '../events.service';
 import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-event-detail',
   templateUrl: './event-detail.page.html',
   styleUrls: ['./event-detail.page.scss'],
 })
-export class EventDetailPage implements OnInit {
+export class EventDetailPage implements OnInit, OnDestroy {
   loadedEvent: Event;
+  private eventSub: Subscription;
+
   constructor(
     private eventService: EventsService,
-    private activedRoute: ActivatedRoute
+    private activedRoute: ActivatedRoute,
+    private navCtrl: NavController
     ) { }
 
   ngOnInit() {
     this.activedRoute.paramMap.subscribe(paramMap => {
       if (!paramMap.has('eventId')) {
+        this.navCtrl.navigateBack('/home');
         return;
       }
-      const eventId = paramMap.get('eventId');
-      this.loadedEvent = this.eventService.getEvent(eventId);
-
+      this.eventSub = this.eventService
+        .getEvent(paramMap.get('eventId'))
+        .subscribe(event => {
+          this.loadedEvent = event;
+        });
     });
+  }
+
+  ngOnDestroy() {
+    if (this.eventSub) {
+      this.eventSub.unsubscribe();
+    }
   }
 
 }
