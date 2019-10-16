97cbdd6928b99652f44579429a0c49e25f148a48
ViniciusRio <viniciustrave@gmail.com>
2019-10-16 19:39:40 -0400

    alterando estrutura de autenticação modificada

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -8,16 +10,49 @@ import { AuthService } from './auth.service';
   styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+  isLoading = false;
+  isLogin = true;
+
   constructor(
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private loadingCtrl: LoadingController
     ) { }
 
   ngOnInit() {
   }
 
   onLogin() {
+    this.isLoading = true;
     this.authService.login();
+    this.loadingCtrl.create({ keyboardClose: true, message: 'Autenticando...' })
+    .then(loadingEl => {
+      loadingEl.present();
+      setTimeout(() => {
+        this.isLoading = false;
+        loadingEl.dismiss();
+        this.router.navigateByUrl('/events');
+      }, 1500);
+    });
     this.router.navigateByUrl('/home');
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
   }
 }
