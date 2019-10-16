cd30a603909eb85798b7c39095156f2ce73baa75
ViniciusRio <viniciustrave@gmail.com>
2019-10-16 19:42:54 -0400

    exibição da tela inicial alterada

--- src/app/home/home-detail/home-detail.page.ts
+++ src/app/home/home-detail/home-detail.page.ts
@@ -1,30 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Event } from '../../events/event.model';
 import { EventsService } from '../../events/events.service';
 import { ActivatedRoute } from '@angular/router';
-import { ModalController } from '@ionic/angular';
+import { ModalController, NavController } from '@ionic/angular';
 import { GetInvitationComponent } from 'src/app/get-invitation/get-invitation.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home-detail',
   templateUrl: './home-detail.page.html',
   styleUrls: ['./home-detail.page.scss'],
 })
-export class HomeDetailPage implements OnInit {
+export class HomeDetailPage implements OnInit, OnDestroy {
   loadedEvent: Event;
+  private eventSub: Subscription;
+
   constructor(
     private eventService: EventsService,
     private activedRoute: ActivatedRoute,
-    private ctrlModal: ModalController
+    private ctrlModal: ModalController,
+    private navCtrl: NavController
+
   ) { }
 
   ngOnInit() {
     this.activedRoute.paramMap.subscribe(paramMap => {
       if (!paramMap.has('eventId')) {
+        this.navCtrl.navigateBack('/home');
         return;
       }
-      const eventId = paramMap.get('eventId');
-      this.loadedEvent = this.eventService.getEvent(eventId);
+      this.eventSub = this.eventService
+        .getEvent(paramMap.get('eventId'))
+        .subscribe(event => {
+          this.loadedEvent = event;
+        });
+
     });
   }
   onGetInvitation() {
@@ -39,5 +49,10 @@ export class HomeDetailPage implements OnInit {
     .then(resultDate => {
       console.log(resultDate.data, resultDate.role)
     });
+  }
+  ngOnDestroy() {
+    if (this.eventSub) {
+      this.eventSub.unsubscribe();
+    }
   }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,22 +2,30 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { EventsService } from '../events/events.service';
 import { Event } from '../events/event.model';
 import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
   loadEvents: Event[];
+  private eventsSub: Subscription;
+
   constructor(
     private eventsService: EventsService,
-    private authService: AuthService
     ) { }
 
   ngOnInit() {
-    this.loadEvents = this.eventsService.getAllEvents();
-    console.log('init ' + this.authService.userAuth);
+    this.eventsSub = this.eventsService.events.subscribe(events => {
+      this.loadEvents = events;
+    });
+  }
 
+  ngOnDestroy() {
+    if (this.eventsSub) {
+      this.eventsSub.unsubscribe();
+    }
   }
 }
