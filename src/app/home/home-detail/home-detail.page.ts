ba73f6d5086bcc8588eb3d911c0a409728d7e91f
ViniciusRio <viniciustrave@gmail.com>
2019-10-22 21:29:18 -0400

    permitindo add o evento na aba eventos atuais, alem de ajustar data

--- src/app/home/home-detail/home-detail.page.ts
+++ src/app/home/home-detail/home-detail.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Event } from '../../events/event.model';
 import { EventsService } from '../../events/events.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ModalController, NavController, LoadingController } from '@ionic/angular';
 import { GetInvitationComponent } from 'src/app/get-invitation/get-invitation.component';
 import { Subscription } from 'rxjs';
@@ -20,8 +20,8 @@ export class HomeDetailPage implements OnInit, OnDestroy {
     private activedRoute: ActivatedRoute,
     private ctrlModal: ModalController,
     private navCtrl: NavController,
-    private loadingCtrl: LoadingController
-
+    private loadingCtrl: LoadingController,
+    private router: Router
   ) { }
 
   ngOnInit() {
@@ -41,7 +41,7 @@ export class HomeDetailPage implements OnInit, OnDestroy {
   onGetInvitation() {
     this.ctrlModal.create({
       component: GetInvitationComponent,
-      componentProps: {eventInvitation: this.loadedEvent}
+      componentProps: {eventInvitation: this.loadedEvent }
     })
     .then(modalElement => {
       modalElement.present();
@@ -49,32 +49,33 @@ export class HomeDetailPage implements OnInit, OnDestroy {
     })
     .then(resultData => {
       console.log(resultData.data, resultData.role);
-
       if (resultData.role === 'confirmed') {
         this.loadingCtrl
           .create({ message: 'Garantindo convite(s)...' })
           .then(loadingElement => {
             loadingElement.present();
             const data = resultData.data.invitationData;
-            // this.bookingService
-            //   .addBooking(
-            //     this.place.id,
-            //     this.place.title,
-            //     this.place.imageUrl,
-            //     data.firstName,
-            //     data.lastName,
-            //     data.guestNumber,
-            //     data.startDate,
-            //     data.endDate
-            //   )
-            //   .subscribe(() => {
-            //     loadingEl.dismiss();
-            //   });
+            this.eventService
+              .addCurrentEvent(
+                this.loadedEvent.name,
+                this.loadedEvent.about,
+                this.loadedEvent.adicionalInformation,
+                this.loadedEvent.entertainment,
+                this.loadedEvent.food,
+                +this.loadedEvent.price,
+                new Date(this.loadedEvent.startDate),
+                new Date(this.loadedEvent.endDate),
+              )
+              .subscribe(() => {
+                loadingElement.dismiss();
+                this.router.navigate(['/events']);
+
+              });
           });
       }
-
     });
   }
+
   ngOnDestroy() {
     if (this.eventSub) {
       this.eventSub.unsubscribe();
