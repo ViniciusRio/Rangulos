94c9458d672e55297f9d7ecdd8bcbae0f30973a4
ViniciusRio <viniciustrave@gmail.com>
2019-10-22 21:26:36 -0400

    ajustado meus eventos, deletando trash

--- src/app/events/my-events/my-events.page.ts
+++ src/app/events/my-events/my-events.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Event } from '../event.model';
-import { EventsService } from '../events.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-events',
@@ -11,21 +9,10 @@ import { Router } from '@angular/router';
 export class MyEventsPage implements OnInit, OnDestroy {
   @Input() myEvents: Event;
   constructor(
-    private eventsService: EventsService,
-    private router: Router
   ) { }
 
-  reMyEvents() {
-    return this.myEvents;
-  }
   ngOnInit() {
   }
-  onDelete(eventId: string) {
-    // this.eventsService.deleteEvent(eventId);
-    this.router.navigate(['/', 'home']);
-
-  }
-
   ngOnDestroy() {
     console.log('onDestroy my events');
   }
