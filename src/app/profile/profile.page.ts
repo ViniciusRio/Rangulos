4c2a0877ee11144f4461c3167852042eecb3902a
ViniciusRio <viniciustrave@gmail.com>
2019-11-09 17:19:26 -0400

    botao de sair

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,3 +1,6 @@
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AuthService } from './../auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,15 +9,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
-  profile;
-  constructor() {
-    this.profile = {
-      name: "Vinicius Rio",
-      email: "viniciustrave@gmail.com",
-      urlPhoto: 'https://picsum.photos/200/300'
-    };
-  }
+  profile = {
+    name: 'Vinicius Rio',
+    email: 'viniciustrave@gmail.com',
+    urlPhoto: 'https://picsum.photos/200/300'
+  };
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private alertCtrl: AlertController
+    ) {}
+
   ngOnInit() {
   }
 
+  onLogout() {
+    this.alertCtrl.create({
+      header: 'Logout',
+      subHeader: 'Deseja voltar para tela de login?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+        },
+        {
+          text: 'Confirmar',
+          cssClass: 'alertDangerColor',
+          handler: () => {
+            this.authService.logout();
+            this.router.navigateByUrl('/auth');
+          }
+        }
+      ]
+    }).then(alertElement => {
+      alertElement.present();
+    });
+  }
 }
