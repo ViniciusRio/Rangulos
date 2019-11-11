b34fc98780ef2d420ac0942f9d6915a04eb57c6c
Vinicius Rio <viniciustrave@gmail.com>
2019-11-11 18:02:59 -0400

    Ajustado constante para buildar no aparelho

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  urlApi: 'http://localhost:8080/api'
+  urlApi: 'http://localhost:8000/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  urlApi: 'http://localhost:8080/api'
+  urlApi: 'http://localhost:8000/api'
 };
 
 /*
